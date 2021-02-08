c1423ffc8ff9d868322b63064eb4759fc75139f5
Jens <jns.bys@gmail.com>
2021-02-08 18:43:19 +0100

    layout finish

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-let api_url = "https://pokeapi.co/";
-async function poke_api(){
-    let api = await fetch(api_url);
-    let data = await api.json();
-    console.log(data);
-}
-poke_api();
+let pokeContainer = document.getElementById("poke-container");
+
+document.getElementById("poke-name").addEventListener("change", function () {
+    let input = document.getElementById("poke-name").value
+    let api_url = `https://pokeapi.co/api/v2/pokemon/${input}`;
+
+    async function poke_api() {
+        let api = await fetch(api_url);
+        let data = await api.json();
+        console.log(data);
+        let pokeID = data.id
+        let evolutionApi = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokeID}/`);
+        let evolutionData = await evolutionApi.json();
+        console.log(evolutionData)
+
+        document.getElementById("poke-photo").src = data.sprites.front_shiny
+
+
+    }
+
+    poke_api();
+
+})
+
+
