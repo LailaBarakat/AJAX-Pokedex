848df742e5680359066a9581901cd226c2c26011
Jens <jns.bys@gmail.com>
2021-02-09 11:17:07 +0100

    changes for fetching pokemon evolution

--- main.js
+++ main.js
@@ -9,17 +9,50 @@ document.getElementById("poke-name").addEventListener("change", function () {
         let data = await api.json();
         console.log(data);
         let pokeID = data.id
-        let evolutionApi = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${pokeID}/`);
+        let species_url = data.species.url;
+        console.log(data.species.url);
+        let evolutionApi = await fetch(species_url);
         let evolutionData = await evolutionApi.json();
         console.log(evolutionData)
+        let pokemon_evol = evolutionData.evolves_from_species;
+        console.log(pokemon_evol);
+        //console.log(pokemon_evol.name);
 
         document.getElementById("poke-photo").src = data.sprites.front_shiny
 
+        document.getElementById("poke-ID").innerHTML = data.id + " " + data.name;
 
+
+        let moves = data.moves;
+
+        document.getElementById("target").innerHTML = ""
+
+
+        for (let i = 0; i<4; i++){
+
+            let clone = template.content.cloneNode(true);
+            clone.querySelector(".move").innerText = moves[i].move.name
+            document.getElementById("target").appendChild(clone)
+        }
+
+        let evolution = document.getElementById("poke-evolution");
+        let evolution_image = document.getElementById("poke_evolve_from");
+        if (pokemon_evol !== null){
+            evolution.innerText = pokemon_evol.name;
+        } else if (pokemon_evol === null) {
+            evolution.innertext = "No previous evolution to show!"
+        }
+
+
+/*
+        if (evolutionData.chain.evolves_to.length > 0){
+            document.getElementById("poke-evolution").innerHTML = evolutionData.chain.evolves_to[0].species.name
+        }    else {
+            document.getElementById("poke-evolution").innerHTML = "no revolutions"
+        }
+*/
     }
 
     poke_api();
 
 })
-
-
